5949d4ab716130bfa0278243c989dfea075d22dd
Domantas <dohxis@gmail.com>
2017-05-05 15:01:23 +0300

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+import 'phoenix_html'
+import '../css/app.css'
+import Elm from '../elm/Main.elm';
+
+Elm.Main.fullscreen();
---
+++ lib/test/web/elm/Main.elm
@@ -0,0 +1,15 @@
+module Main exposing (..)
+
+import Html exposing (..)
+
+
+main : Html msg
+main =
+    div []
+        [ text "Hello From Elm From the HelloElm.elm file !"
+        , br [] []
+        , br [] []
+        , text "Now let try to import another file ..."
+        , br [] []
+        , br [] []
+        ]
